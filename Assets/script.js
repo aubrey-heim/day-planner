f53d330935dc5e79ed71687bd7c0d24e68d18c3b
aubrey-heim <aubrey.l.heim@gmail.com>
2020-10-03 15:54:22 -0700

    creating save functionality

--- Assets/script.js
+++ Assets/script.js
@@ -2,12 +2,22 @@ let dayTime = moment().format("dddd, MMMM Do YYYY, h:mm a");
 let hour = moment().format("H");
 let hourBlocks = [$("#9"), $("#10"), $("#11"), $("#12"), $("#13"), $("#14"), $("#15"), $("#16"), $("#17")]
 
+let savedText = JSON.parse(localStorage.getItem("savedText"));
+
+if (savedText) {
+    updateText()
+} else {
+    savedText = ["", "", "", "", "", "", "", "", ""]
+}
+
+
+
 $("#currentDay").text(dayTime)
 
 hourBlocks.forEach(element => checkHour(element))
+
     
 function checkHour(element){
-    console.log(element.attr("id"))
     var hourBlock = parseInt(element.attr("id"))
     if (hourBlock<hour){
         element.addClass("past")
@@ -17,5 +27,21 @@ function checkHour(element){
         element.addClass("present")
     }
 }
-        
-    
+
+function updateText (){
+    for (let i=0; i<9; i++){
+        let selectedBlock = $(".container").children(0)[i]
+        let selectedTextArea = selectedBlock.children[1]
+        selectedTextArea.textContent = savedText[i]
+
+    }
+}
+
+$(".saveBtn").on("click", function(event){
+    event.preventDefault()
+    let inputText = (event.target.previousSibling.previousSibling.value)
+    let blockChanged = (event.target.parentElement)
+    let hourChanged = blockChanged.getAttribute("id")
+    savedText[hourChanged-9] = inputText
+    localStorage.setItem("savedText", JSON.stringify(savedText))
+})
